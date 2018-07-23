3b246a48b29398c144797af52ad5d4347ea2c95b
Alan Gregos <alangregos1983@gmail.com>
2018-07-23 16:08:29 -0400

    Initial commit with radio button UI constructed.

---
+++ app/src/main/java/com/example/alangregos/lordoftheringsnerdquiz/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.alangregos.lordoftheringsnerdquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+// question answers need to be 40 chars or less
+
+}
