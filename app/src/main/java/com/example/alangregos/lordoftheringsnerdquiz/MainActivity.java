5ea5c64851f729e1c8d695ca6244530e802fb3a2
Alan Gregos <alangregos1983@gmail.com>
2018-07-24 21:08:16 -0400

    restrucred Question Class to be more efficient and removed the nested Answer class. Made adjustments to the already written

--- app/src/main/java/com/example/alangregos/lordoftheringsnerdquiz/MainActivity.java
+++ app/src/main/java/com/example/alangregos/lordoftheringsnerdquiz/MainActivity.java
@@ -2,27 +2,28 @@ package com.example.alangregos.lordoftheringsnerdquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Question question1 = new Question("What is the name of the weapon shown in this picture?", "Glamdring", false, "Orcrist",
-            false, "Sting", false, "Anduril", true, "Morgul Blade", false, "Herugrim", false, R.drawable.Anduril);
-    private Question question2 = new Question("What is the name of the weapon shown in this picture?", "Glamdring", true, "Orcrist",
-            false, "Sting", false, "Anduril", false, "Morgul Blade", false, "Herugrim", false, R.drawable.Glamdring);
-    private Question question3 = new Question("What is the name of the weapon shown in this picture?", "Glamdring", false, "Orcrist",
-            false, "Sting", true, "Anduril", false, "Morgul Blade", false, "Herugrim", false, R.drawable.LOTR_Sting);
-    private Question question4 = new Question("What is the name of the weapon shown in this picture?", "Glamdring", false, "Orcrist",
-            false, "Sting", false, "Anduril", false, "Morgul Blade", true, "Herugrim", false, R.drawable.Morgul_Blade);
-    private Question question5 = new Question("What is the name of the weapon shown in this picture", "Glamdring", false, "Orcrist",
-            true, "Sting", false, "Anduril", false, "Morgul Blade", false, "Herugrim", false, R.drawable.Orcrist);
+    private Question question1 = new Question("What is the name of the weapon shown in this picture?", "Anduril", "Glamdring", "Orcrist",
+            "Sting", "Anduril", "Morgul Blade", "Herugrim", R.drawable.Anduril);
+    private Question question2 = new Question("What is the name of the weapon shown in this picture?", "Glamdring", "Glamdring", "Orcrist",
+            "Sting", "Anduril", "Morgul Blade", "Herugrim", R.drawable.Glamdring);
+    private Question question3 = new Question("What is the name of the weapon shown in this picture?", "Sting", "Glamdring", "Orcrist",
+            "Sting", "Anduril", "Morgul Blade", "Herugrim", R.drawable.LOTR_Sting);
+    private Question question4 = new Question("What is the name of the weapon shown in this picture?", "Morgul Blade", "Glamdring", "Orcrist",
+            "Sting", "Anduril", "Morgul Blade", "Herugrim", R.drawable.Morgul_Blade);
+    private Question question5 = new Question("What is the name of the weapon shown in this picture","Orcrist", "Glamdring",  "Orcrist",
+             "Sting",  "Anduril",  "Morgul Blade",  "Herugrim",  R.drawable.Orcrist);
 
-    private Question question6 = new Question("Who fought wielding this weapon?", "Bilbo", true, "Aaragorn", false,
-            "Gandalf", false, "Leglas", false, "Thranduil", false, "Frodo", false, R.drawable.The_Hobbit_Sting);
-    private Question question7 = new Question("Who fought wielding this weapon?", "Bilbo", false, "Aragorn", false,
-            "Gandalf", false, "Legolas", true, "Thranduil", false, "Thorin", true, R.drawable.Orcrist);
-    private Question question8 = new Question("Who are descended from kings?", "Bilbo", false, "Aragorn", true,
-            "Gandalf", false, "Legolas", true, "Thranduil", true, "Faramir", false, R.drawable.throne);
+    private Question question6 = new Question("Who fought wielding this weapon?", "Bilbo", "Bilbo",  "Aaragorn",
+            "Gandalf",  "Leglas",  "Thranduil",  "Frodo",  R.drawable.The_Hobbit_Sting);
+    private Question question7 = new Question("Who fought wielding this weapon?","","","", "Bilbo",  "Aragorn",
+            "Gandalf",  "Legolas",  "Thranduil",  "Thorin",  R.drawable.Orcrist);
+    private Question question8 = new Question("Who are descended from kings?", "Aragorn","Legolas","Thranduil","Bilbo",  "Aragorn",
+            "Gandalf",  "Legolas",  "Thranduil",  "Faramir",  R.drawable.throne);
 
     private Question question9 = new Question("What is the name of the battering ram from Return of the King?", "Grond", R.drawable.Grond);
     private Question question10 = new Question("What is the name of Gandalf's horse ", "Shadowfax", R.drawable.Shadowfax);
@@ -41,6 +42,18 @@ public class MainActivity extends AppCompatActivity {
     // TODO: 7/23/2018 script radio buttons and link to questions and answers
     // TODO: 7/23/2018 script check boxes and link to questions and answers
     // TODO: 7/23/2018script edit text and link to questions and answers
+
+    public void changeQuestion(String message) {
+
+        TextView questionTextView = findViewById(R.id.question);
+        questionTextView.setText(message);
+    }
+
+    public void changeAnswerOne(String message) {
+        TextView answerTextView = findViewById(R.id.radio_answer_one);
+        TextView checkBoxAnswer = findViewById(R.id.check_box_1);
+        answerTextView.setText(message);
+    }
 
 
 }
--- app/src/main/java/com/example/alangregos/lordoftheringsnerdquiz/Question.java
+++ app/src/main/java/com/example/alangregos/lordoftheringsnerdquiz/Question.java
@@ -3,59 +3,53 @@ package com.example.alangregos.lordoftheringsnerdquiz;
 public class Question {
 
     String questionText;
-    String answerText;
-    Answer answer1;
-    Answer answer2;
-    Answer answer3;
-    Answer answer4;
-    Answer answer5;
-    Answer answer6;
+    int numberOfCorrectAnswers;
+    String correctAnswerTextOne;
+    String correctAnswerTextTwo;
+    String correctAnswerTextThree;
+    String answer1;
+    String answer2;
+    String answer3;
+    String answer4;
+    String answer5;
+    String answer6;
     int image;
 
-
-    //Nested class for coding the answers. Moved hear because it would only ever be used in conjunction with a question and isn't very mich code.
-    public class Answer {
-
-        private String answerText;
-        private boolean isCorrect;
-        private String textAnswer;
-
-        public Answer(String text, boolean right) {
-            this.answerText = text;
-            this.isCorrect = right;
-        }
-
-        public Answer(String text) {
-            this.textAnswer = text;
-        }
-
-        public boolean checkIsCorrect() {
-            return isCorrect;
-        }
-
-        public String getTextAnswer() {
-            return this.textAnswer;
-        }
+    //constructor for questions that only have 1 correct answer.
+    public Question(String question, String correct, String text1, String text2, String text3, String text4,
+                    String text5, String text6, int imageToDisplay) {
+        this.questionText = question;
+        this.correctAnswerTextOne = correct;
+        this.answer1 = text1;
+        this.answer2 = text2;
+        this.answer3 = text3;
+        this.answer4 = text4;
+        this.answer5 = text5;
+        this.answer6 = text6;
+        this.image = imageToDisplay;
 
     }
 
-
-    public Question(String question, String text1, boolean correct1, String text2, boolean correct2, String text3, boolean correct3, String text4,
-                    boolean correct4, String text5, boolean correct5, String text6, boolean correct6, int imageToDisplay) {
+    // constructor for check boxes that supports up to 3 correct answers.
+    public Question(String question, String correct, String correctAlso, String correctToo, String text1, String text2, String text3, String text4,
+                    String text5, String text6, int imageToDisplay) {
         this.questionText = question;
-        this.answer1 = new Answer(text1, correct1);
-        this.answer2 = new Answer(text2, correct2);
-        this.answer3 = new Answer(text3, correct3);
-        this.answer4 = new Answer(text4, correct4);
-        this.answer5 = new Answer(text5, correct5);
-        this.answer6 = new Answer(text6, correct6);
+        this.correctAnswerTextOne = correctAlso;
+        this.correctAnswerTextTwo = correctToo;
+        this.correctAnswerTextThree = correct;
+        this.answer1 = text1;
+        this.answer2 = text2;
+        this.answer3 = text3;
+        this.answer4 = text4;
+        this.answer5 = text5;
+        this.answer6 = text6;
         this.image = imageToDisplay;
 
     }
 
     public Question(String question, String correctAnswer, int imageToDisplay) {
         this.questionText = question;
-        this.answerText = correctAnswer;
+        this.correctAnswerTextOne = correctAnswer;
         this.image = imageToDisplay;
     }
 
